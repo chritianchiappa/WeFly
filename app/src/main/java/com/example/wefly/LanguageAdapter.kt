fb5b544b463ecb5e8ecd327e6c2e309a0d619782
michele <carlonimichele3008@gmail.com>
2024-05-19 17:35:16 +0200

    MODIFICA - 3

--- app/src/main/java/com/example/wefly/LanguageAdapter.kt
+++ app/src/main/java/com/example/wefly/LanguageAdapter.kt
@@ -1,28 +1,37 @@
 package com.example.wefly
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import android.widget.TextView
 
-class LanguageAdapter(val data: List<LanguageData>) : RecyclerView.Adapter<LanguageAdapter.LanguageViewHolder>(){
+class LanguageAdapter(var mList: List<LanguageData>) : RecyclerView.Adapter<LanguageAdapter.LanguageViewHolder>(){
 
     inner class LanguageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
         val logo : ImageView = itemView.findViewById(R.id.logoIv)
         val titleTv : TextView = itemView.findViewById(R.id.titleTv)
     }
 
+    fun setFilteredList(mList: List<LanguageData>){
+        this.mList = mList
+        notifyDataSetChanged()
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LanguageViewHolder {
-        TODO("Not yet implemented")
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.each_item, parent, false)
+        return LanguageViewHolder(view)
+    }
+
+
+    override fun onBindViewHolder(holder: LanguageViewHolder, position: Int) {
+        holder.logo.setImageResource(mList[position].logo)
+        holder.titleTv.text = mList[position].title
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
-
-    override fun onBindViewHolder(holder: LanguageViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        return mList.size
     }
 
 }
--- app/src/main/java/com/example/wefly/ViaggiFragment.kt
+++ app/src/main/java/com/example/wefly/ViaggiFragment.kt
@@ -6,13 +6,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.SearchView
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import java.util.Locale
 
 
 class ViaggiFragment : Fragment() {
     private lateinit var recyclerView: RecyclerView
     private lateinit var searchView: SearchView
+    private var mList = ArrayList<LanguageData>()
+    private lateinit var adapter: LanguageAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,12 +40,45 @@ class ViaggiFragment : Fragment() {
 
         // Setup RecyclerView
         recyclerView.setHasFixedSize(true)
-        recyclerView.layoutManager = LinearLayoutManager(context) // Use context or requireContext()
-
+        recyclerView.layoutManager = LinearLayoutManager(requireContext()) // Use context or requireContext()
         addDataToList()
+        adapter = LanguageAdapter(mList)
+        recyclerView.adapter = adapter
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                filterList(newText)
+                return true
+            }
+
+        })
+    }
+
+    private fun filterList(query : String?){
+        if (query != null) {
+            val filteredList = ArrayList<LanguageData>()
+            for (i in mList) {
+                if (i.title.lowercase(Locale.ROOT).contains(query)) {
+                    filteredList.add(i)
+                }
+            }
+
+            if (filteredList.isEmpty()){
+                Toast.makeText(requireContext(), "No Data Found", Toast.LENGTH_SHORT).show()
+            } else {
+                adapter.setFilteredList(filteredList)
+            }
+        }
     }
 
     private fun addDataToList() {
-        // Your code to add data to the list
+        mList.add(LanguageData("Java", R.drawable.logo_png))
+        mList.add(LanguageData("Java", R.drawable.logo_png))
+        mList.add(LanguageData("Java", R.drawable.logo_png))
+
     }
 }
