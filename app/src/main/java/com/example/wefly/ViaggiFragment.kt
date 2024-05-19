bb399923bb7dc99b8481ca69165115333a888f67
michele <carlonimichele3008@gmail.com>
2024-05-19 16:25:12 +0200

    MODIFICA - 2

--- app/src/main/java/com/example/wefly/ViaggiFragment.kt
+++ app/src/main/java/com/example/wefly/ViaggiFragment.kt
@@ -5,16 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 
 class ViaggiFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var searchView: SearchView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-        }
+        // Initial setup that doesn't require view references can go here
     }
 
     override fun onCreateView(
@@ -25,4 +27,21 @@ class ViaggiFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_viaggi, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Initialize views here
+        recyclerView = view.findViewById(R.id.recyclerView)
+        searchView = view.findViewById(R.id.searchView)
+
+        // Setup RecyclerView
+        recyclerView.setHasFixedSize(true)
+        recyclerView.layoutManager = LinearLayoutManager(context) // Use context or requireContext()
+
+        addDataToList()
+    }
+
+    private fun addDataToList() {
+        // Your code to add data to the list
+    }
 }
